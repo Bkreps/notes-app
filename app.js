a3641e2e9bfe55b67d275147529fbf2d476b5b84
Bkreps <krepsbryan@gmail.com>
2019-08-17 23:35:56 -0400

    inital commit

---
+++ app.js
@@ -0,0 +1,61 @@
+const chalk = require('chalk');
+const yargs = require('yargs');
+const getNotes = require('./notes.js');
+
+yargs.version('1.1.0');
+
+
+//Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: "Note title",
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: "Note body",
+            demandOption: true,
+            type: 'string'
+        },
+    },
+    handler: function (argv) {
+        console.log('Title: ' + argv.title);
+        console.log('Body: ' + argv.body);
+    }
+});
+
+//create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing the note');
+    }
+});
+
+//create list command
+yargs.command({
+    command: 'list',
+    describe: 'list the notes',
+    builder: {
+
+    },
+    handler: function () {
+        console.log('Listing out notes');
+    }
+});
+
+//create read command
+yargs.command({
+    command: 'read',
+    describe: 'read a note',
+    handler: function () {
+        console.log('Reading the note');
+    }
+});
+
+yargs.parse();
+
